1168c9887c1ea599fb333f16557a79802c66ee8b
Bastien Winant <bastien.winant@gmail.com>
2024-07-28 01:39:45 +0200

    complete HW2

--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -71,10 +71,20 @@ def run_register_model(data_path: str, top_n: int):
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids='1',
+        filter_string="",
+        run_view_type=ViewType.ACTIVE_ONLY,
+        order_by=["metrics.test_rmse ASC"]
+    )[0]
 
     # Register the best model
-    # mlflow.register_model( ... )
+    run_id = best_run.info.run_id
+    model_uri = f"runs:/{run_id}/model"
+    mlflow.register_model(
+        model_uri=model_uri,
+        name="nyc-taxi-regressor"
+    )
 
 
 if __name__ == '__main__':
